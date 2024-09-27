f91e4c7425691a3b33b8998952d9289e4286d760
auddnjs2008 <auddnjs2008@gmail.com>
2024-09-27 23:44:25 +0900

    e2e login

--- test/users.e2e-spec.ts
+++ test/users.e2e-spec.ts
@@ -2,10 +2,25 @@ import { Test, TestingModule } from '@nestjs/testing';
 import { INestApplication } from '@nestjs/common';
 import * as request from 'supertest';
 import { AppModule } from '../src/app.module';
+import { DataSource } from 'typeorm';
+
+
+global.fetch = jest.fn(() => Promise.resolve({
+  json: () => Promise.resolve({success:'success'})
+})) as jest.Mock;
+
+
+const testUser = {
+  email :  'new3@naver.com',
+  password :'auddnjs152!'
+}
+
+
+const GRAPHQL_ENDPOINT = '/graphql';
 
 describe('UserModule (e2e)', () => {
   let app: INestApplication;
-
+  let jwtToken :string;
   beforeAll(async () => {
     const module: TestingModule = await Test.createTestingModule({
       imports: [AppModule],
@@ -15,11 +30,135 @@ describe('UserModule (e2e)', () => {
     await app.init();
   });
 
-  it.todo('me');
+  afterAll(async () => {
+    const dataSource : DataSource = new DataSource({
+      type:'postgres',
+      host:process.env.DB_HOST,
+      port:+process.env.DB_PORT,
+      username:process.env.DB_USERNAME,
+      password:process.env.DB_PASSWORD,
+      database:process.env.DB_NAME,
+    });
+    const connection:DataSource = await dataSource.initialize();
+    await connection.dropDatabase(); // database delete
+    await connection.destroy();
+    await app.close();
+  })
+
+  describe("createAccount",()=>{
+   
+      it('should create account', () => {
+         return request(app.getHttpServer()).post(GRAPHQL_ENDPOINT).send({
+          query: `
+          mutation{
+            createAccount(input:
+              {email:"${testUser.email}",password:"${testUser.password}",role:Owner}
+            ){
+              ok
+              error
+             }
+            }         
+          `
+         }).expect(200).expect(async res => {
+            expect(res.body.data.createAccount.ok).toBe(true);
+            expect(res.body.data.createAccount.error).toBe(null);
+         });
+      })
+
+
+      it('should fail if account already exists', () => {
+        return request(app.getHttpServer()).post(GRAPHQL_ENDPOINT).send({
+          query: `
+          mutation{
+            createAccount(input:
+              {email:"${testUser.email}",password:"${testUser.password}",role:Owner}
+            ){
+              ok
+              error
+             }
+            }         
+          `
+         }).expect(200)
+         .expect(res => {
+          expect(res.body.data.createAccount.ok).toBe(false);
+            expect(res.body.data.createAccount.error).toEqual(expect.any(String));
+         })
+
+
+
+      });
+
+  }); 
+
+  describe("login", () => {
+    it('should login with correct credentials', () => {
+      return request(app.getHttpServer())
+        .post(GRAPHQL_ENDPOINT)
+        .send({
+          query: `
+          mutation {
+            login(input:{
+              email:"${testUser.email}",
+              password:"${testUser.password}",
+            }) {
+              ok
+              error
+              token
+            }
+          }
+        `,
+        })
+        .expect(200)
+        .expect(res => {
+          const {
+            body: {
+              data: { login },
+            },
+          } = res;
+          expect(login.ok).toBe(true);
+          expect(login.error).toBe(null);
+          expect(login.token).toEqual(expect.any(String));
+          jwtToken = login.token;
+        
+        });
+    });
+
+    it('should not be able to login with wrong credentials',() => {
+      return request(app.getHttpServer())
+        .post(GRAPHQL_ENDPOINT)
+        .send({
+          query: `
+          mutation {
+            login(input:{
+              email:"${testUser.email}",
+              password:"xxx",
+            }) {
+              ok
+              error
+              token
+            }
+          }
+        `,
+        })
+        .expect(200)
+        .expect(res => {
+          const {
+            body: {
+              data: { login },
+            },
+          } = res;
+          expect(login.ok).toBe(false);
+          expect(login.error).toBe('Wrong password');
+          expect(login.token).toBe(null);
+        });
+    })
+
+
+  }); 
+
   it.todo("userProfile");  
-  it.todo("createAccount"); 
-  it.todo("login"); 
-  it.todo("editProfile");
+  it.todo('me');
   it.todo("verifyEmail");
+  it.todo("editProfile");
 
 });
